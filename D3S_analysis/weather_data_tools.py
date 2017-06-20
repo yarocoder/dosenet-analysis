1882bc85964c5604660b8eae8206502e4e5dc117
alihanks <alihanks@gmail.com>
2017-06-20 15:00:27 -0700

    reworked fitting tools

--- D3S_analysis/weather_data_tools.py
+++ D3S_analysis/weather_data_tools.py
@@ -1,6 +1,14 @@
 import csv
 import io
-import urllib.request
+
+# Python 2 and 3: easiest option
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -8,26 +16,27 @@ import matplotlib.dates as mdates
 from datetime import datetime
 from datetime import timedelta
 from matplotlib.dates import date2num
-from statistic import *
 
 def weather_station_data_scrape(ID, date):
-	'''
+    '''
     Scrap weather data of given location and given period of time from websites
     
     Arguments:
-	    - ID is a string contains weather station ID
+        - ID is a string contains weather station ID
     	- date is a 1 by 3 string array: Month/Date/Year
     '''
 
-    data_temp=[] #Stores raw data from the weather station
+    data_temp=[]
     str1 = 'https://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID='
     str2 = '&day='
     str3 = '&month='
     str4 = '&year='
     str5 = '&graphspan=day&format=1'
-    url = str1+ID+str2+date[1]+str3+date[0]+str4+date[2]+str5
-    response = urllib.request.urlopen(url)
-    cr=csv.reader(io.TextIOWrapper(response))
+    url = str1+ID+str2+str(date.day)+str3+str(date.month)\
+          +str4+str(date.year)+str5
+    response = urlopen(url)
+    cr = csv.reader(response)
+    #cr=csv.reader(io.TextIOWrapper(response))
     for row in cr:
         if len(row)<= 1: continue
         data_temp.append(row)
@@ -45,22 +54,19 @@ def weather_station_data_scrape(ID, date):
             data[i][data_temp[0][:].index('WindDirection')+1:data_temp[0][:].index('Conditions')] = tuple(float(list_item) for list_item in data_temp[i][data_temp[0][:].index('WindDirection')+1:data_temp[0][:].index('Conditions')])
             data[i][data_temp[0][:].index('Conditions'):data_temp[0][:].index('Clouds')+1] = data_temp[i][data_temp[0][:].index('Conditions'):data_temp[0][:].index('Clouds')+1]
             data[i][data_temp[0][:].index('Clouds')+1:len(data_temp[0][:])-2] = tuple(float(list_item) for list_item in data_temp[i][data_temp[0][:].index('Clouds')+1:len(data_temp[i][:])-3])
-    
-    #Select data for csv file(Date,Temperature, Pressure, Windspeed, Humidity, Hourly Precipitation, and Solar radiation)
-    data_csv = [[0 for i in range(7)] for j in range(len(data))]
+
+    #Select data for output array: 
+    #   (Date,Temperature, Pressure, Windspeed, Humidity, 
+    #    Hourly Precipitation, and Solar radiation)
+    data_out = [[0 for i in range(7)] for j in range(len(data))]
 
     for i in range(len(data_temp)):
-        data_csv[i][0:2] = data[i][0:2]
-        data_csv[i][2] = data[i][3]
-        data_csv[i][3] = data[i][6]
-        data_csv[i][4:6] = data[i][8:10]
-        data_csv[i][6] = data[i][13]
+        data_out[i][0:2] = data[i][0:2]
+        data_out[i][2] = data[i][3]
+        data_out[i][3] = data[i][6]
+        data_out[i][4:6] = data[i][8:10]
+        data_out[i][6] = data[i][12]
     
-    #Store data as a csv file locally (in current directory)
-    path = os.getcwd() + ID + '.csv'
-    with open(path,'w',newline='') as f:
-        csv_file = csv.writer(f, delimiter=',')
-        for row in data_csv:
-            csv_file.writerow(row)
-
-    return data_csv, csv_file
+    # remove meta data
+    data_out.pop(0)
+    return data_out
